3510ae9388fbc9cce9c584ac30951cfcc716ef2b
Sharayu09Patil <103387547+Sharayu09Patil@users.noreply.github.com>
2022-04-10 22:19:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
+#,
+
+#import
+#website_app
+
+
 from website import create_app
 
 # app = create_app()
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
+
+#.
+#import 
+#os
+
 import os
 # from website import create_app
 
+
+#assigning create_app to app
+
+
 app = create_app()
+
+
+#main
 
 if __name__ == '__main__':
     server_port = os.environ.get('PORT', '8080')
